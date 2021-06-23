49a8069ce28b3959910a58736109dad099b27728
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-23 19:36:07 +0200

    miatoll: Add libinit for variants handling
    [dereference23: Adapt for miatoll]
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,11 @@ DEVICE_PATH := device/xiaomi/miatoll
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_miatoll
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_miatoll
+
+
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := curtana,excalibur,gram,joyeuse,miatoll
 
---
+++ libinit/init_xiaomi_miatoll.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
+
+#include "vendor_init.h"
+
+static const variant_info_t curtanain_info = {
+    .hwc_value = "India",
+
+    .brand = "Redmi",
+    .device = "curtana",
+    .model = "Redmi Note 9 Pro",
+    .nfc = false,
+};
+
+static const variant_info_t curtana_info = {
+    .hwc_value = "",
+
+    .brand = "Redmi",
+    .device = "curtana",
+    .model = "Redmi Note 9S",
+    .nfc = false,
+};
+
+static const variant_info_t excalibur_info = {
+    .hwc_value = "",
+
+    .brand = "Redmi",
+    .device = "excalibur",
+    .model = "Redmi Note 9 Pro Max",
+    .nfc = false,
+};
+
+static const variant_info_t gram_info = {
+    .hwc_value = "",
+
+    .brand = "POCO",
+    .device = "gram",
+    .model = "POCO M2 Pro",
+    .nfc = false,
+};
+
+static const variant_info_t joyeuse_info = {
+    .hwc_value = "",
+
+    .brand = "Redmi",
+    .device = "joyeuse",
+    .model = "Redmi Note 9 Pro",
+    .nfc = true,
+};
+
+static const std::vector<variant_info_t> variants = {
+    curtanain_info,
+    curtana_info,
+    excalibur_info,
+    gram_info,
+    joyeuse_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+    set_dalvik_heap();
+}
